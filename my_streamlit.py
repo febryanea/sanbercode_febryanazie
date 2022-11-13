157ea1ea6aaab97e96ae8ac7a3a4d7231ae7182e
Febryan Elfanuary Azie <febryanazie@gmail.com>
2022-11-14 05:47:01 +0700

    test 3

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import json, requests, re
+import tweepy
+import string
+import requests 
+from bs4 import BeautifulSoup 
+from urllib.request import urlopen
+import preprocessor as p
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+
 
 option = st.sidebar.selectbox(
     'Silakan pilih:',
@@ -10,13 +26,9 @@ option = st.sidebar.selectbox(
 if option == 'Home' or option == '':
     st.write("""# Halaman Utama""") #menampilkan halaman utama
 elif option == 'Dataframe':
-    st.write("""## Dataframe""") #menampilkan judul halaman dataframe
+    st.write("""## Data Source""") #menampilkan judul halaman dataframe
+    df = pd.read_csv('news_resesi.csv')
 
-    #membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data
-    df = pd.DataFrame({
-        'Column 1':[1,2,3,4],
-        'Column 2':[10,12,14,16]
-    })
     df #menampilkan dataframe
 elif option == 'Chart':
     st.write("""## Draw Charts""") #menampilkan judul halaman 
