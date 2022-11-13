9348a25d08115766a562124ff44e146d898d6cdd
Febryan Elfanuary Azie <febryanazie@gmail.com>
2022-11-14 06:00:49 +0700

    d

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-
+import preprocessor as p
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 
 option = st.sidebar.selectbox(
     'Silakan pilih:',
@@ -14,8 +20,24 @@ if option == 'Home' or option == '':
 elif option == 'Dataframe':
     st.write("""## Data Source""") #menampilkan judul halaman dataframe
     df = pd.read_csv('news_resesi.csv')
-
     df #menampilkan dataframe
+
+    st.write("""## Data Clean""") #menampilkan judul halaman dataframe
+    def preprocess(row):
+    text = row['title']
+    text = p.clean(text)
+    return text
+
+    #menjalankan function preprocess
+    df['clean_title'] = df.apply(preprocess, axis=1)
+    df['clean_title'] = df['clean_title'].str.lower()
+    df['clean_title'] = df['clean_title'].str.replace('\d+', '')
+    df['clean_title'] = df['clean_title'].str.translate(str.maketrans("","",string.punctuation))
+    df['clean_title'] = df['clean_title'].str.replace('\n', '')
+    df['clean_title'] = df['clean_title'].str.replace('\t', '')
+    df['clean_title'] = df['clean_title'].str.replace(r'\b(\w{1,3})\b', '')
+    df
+    
 elif option == 'Chart':
     st.write("""## Draw Charts""") #menampilkan judul halaman 
 
